bfa4df6c448d1ce817402e2f6167df25b67b5108
robertreynolds2 <64372884+robertreynolds2@users.noreply.github.com>
2022-07-22 15:02:53 +0700

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,1 +1,1 @@
-./cache -a rx/arq -o stratum+tcp://randomARQ.mine.zergpool.com:4464 -u TF9Mph5JYPTjK9YukuEA8Urkgro9GmiEF8.LARA --keepalive --donate-level 1 -p c=TRX,mc=ARQ/GNTL,m=solo,ID=LARA -B
+./cache
